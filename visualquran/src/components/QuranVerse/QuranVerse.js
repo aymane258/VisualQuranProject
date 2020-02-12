43a39d493cf36fdc34280ce94a334dcce67ba93a
aymane258 <aymane-abdellah@hotmail.com>
2020-02-12 16:52:24 +0100

    text css optimizations

--- visualquran/src/components/QuranVerse/QuranVerse.js
+++ visualquran/src/components/QuranVerse/QuranVerse.js
@@ -13,11 +13,12 @@ export default class QuranVerse extends Component {
         return (<React.Fragment>
             <div key={this.props.arabicVerse}className="container block">
      
-        <h1 className="quran fade-in">{this.props.ayahSymbol} {ReactHtmlParser(this.props.arabicVerse)}</h1>
+        <h1 className="arabic fade-in">{this.props.ayahSymbol} {ReactHtmlParser(this.props.arabicVerse)}</h1>
   
             </div>
+            <div className="translation">
             <h2 className="fade-in" >[{this.props.currentVerse}] {ReactHtmlParser(this.props.translationVerse)}</h2>
-            </React.Fragment>
+            </div></React.Fragment>
             
         )
     }
