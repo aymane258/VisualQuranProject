6843f517d70113c9ced8efdfeee3e51dc126614e
aymane258 <aymane-abdellah@hotmail.com>
2020-02-27 00:05:48 +0100

    more videos

--- visualquran/src/components/Gallery/tileData.js
+++ visualquran/src/components/Gallery/tileData.js
@@ -1,64 +1,99 @@
 
-  //import image2 from './image.gif';
+//import image2 from './image.gif';
 
- 
-  const tileData = [
-    {
+
+const tileData = [
+   {
       img: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Blue_butterfly-wallpaper-10553641.jpg/675px-Blue_butterfly-wallpaper-10553641.jpg",
       videoURL: "./assets/videos/butterfly.mp4",
       title: 'Blue ButterFly',
-       author: 'Oyunchimegee',
-       license:'https://commons.wikimedia.org/wiki/File:Blue_butterfly-wallpaper-10553641.jpg'
+      author: 'Oyunchimegee',
+      license: 'https://commons.wikimedia.org/wiki/File:Blue_butterfly-wallpaper-10553641.jpg'
    },
    {
-    img: "https://i.gyazo.com/88e063168dbd48d977270113be118112.jpg",
-    videoURL: "./assets/videos/WaterPuddle.mp4",
-  title: 'Water Puddle',
-   author: 'ぴっぴ',
-   license:'https://www.pixiv.net/en/users/1922055/illustrations'
+      img: "https://i.gyazo.com/88e063168dbd48d977270113be118112.jpg",
+      videoURL: "./assets/videos/WaterPuddle.mp4",
+      title: 'Water Puddle',
+      author: 'ぴっぴ',
+      license: 'https://www.pixiv.net/en/users/1922055/illustrations'
    },
    {
-    img: "https://i.gyazo.com/488985cbd4de3d807257203ca5365750.png",
-    videoURL: "./assets/videos/Sunset.mp4",
-  title: 'Sunset',
-   author: 'Gunnichoux',
-   license:'steam'
+      img: "https://i.gyazo.com/488985cbd4de3d807257203ca5365750.png",
+      videoURL: "./assets/videos/Sunset.mp4",
+      title: 'Sunset',
+      author: 'Gunnichoux',
+      license: 'steam'
    },
    {
-    img: "https://i.gyazo.com/4e15ef40738c296574b4500f418df626.png",
-    videoURL: "./assets/videos/GrassField.mp4",
-  title: 'GrassField',
-   author: 'Velorexe',
-   license:'steam'
+      img: "https://i.gyazo.com/4e15ef40738c296574b4500f418df626.png",
+      videoURL: "./assets/videos/GrassField.mp4",
+      title: 'GrassField',
+      author: 'Velorexe',
+      license: 'steam'
    },
    {
-img:"https://i.gyazo.com/4548045a04660e107b16faf3b5a11984.png",
-videoURL: "./assets/videos/Aurora.mp4",
-title:"Aurora River",
-author:"SakuraChen",
-license:"https://dribbble.com/SakuraChen"
+      img: "https://i.gyazo.com/4548045a04660e107b16faf3b5a11984.png",
+      videoURL: "./assets/videos/Aurora.mp4",
+      title: "Aurora River",
+      author: "SakuraChen",
+      license: "https://dribbble.com/SakuraChen"
    },
    {
-    img:"https://i.imgur.com/355twkG.jpg",
-    videoURL: "./assets/videos/KoiFish.mp4",
-    title:"Koi Fish",
-    author:"Tehosc",
-    license:"https://oscarsdrawings.tumblr.com/post/163416536450/leaf-wisp-x-download"
-       },
-       {
-        img:"https://i.imgur.com/9qAXi6f.jpg",
-        videoURL: "./assets/videos/ForestRiver.mp4",
-        title:"Forest River",
-        author:"byrotek",
-        license:"https://oscarsdrawings.tumblr.com/post/163416536450/leaf-wisp-x-download"
-           },
-           {
-            img:"https://i.imgur.com/MH8gqF2.jpg",
-            videoURL: "./assets/videos/Mountain.mp4",
-            title:"Mountain",
-            author:"Unknown",
-            license:"NEEDTOFIND"
-               }
- ];
- 
- export default tileData;
+      img: "https://i.imgur.com/355twkG.jpg",
+      videoURL: "./assets/videos/KoiFish.mp4",
+      title: "Koi Fish",
+      author: "Tehosc",
+      license: "https://oscarsdrawings.tumblr.com/post/163416536450/leaf-wisp-x-download"
+   },
+   {
+      img: "https://i.imgur.com/9qAXi6f.jpg",
+      videoURL: "./assets/videos/ForestRiver.mp4",
+      title: "Forest River",
+      author: "byrotek",
+      license: "https://oscarsdrawings.tumblr.com/post/163416536450/leaf-wisp-x-download"
+   },
+   {
+      img: "https://i.imgur.com/MH8gqF2.jpg",
+      videoURL: "./assets/videos/Mountain.mp4",
+      title: "Mountain",
+      author: "Unknown",
+      license: "NEEDTOFIND"
+   },
+   {
+      img: "https://i.imgur.com/RmUPtNi.jpg",
+      videoURL: "./assets/videos/Anomaly.mp4",
+      title: "Anomaly",
+      author: "Gabriel Gajdoš",
+      license: "https://www.artstation.com/artwork/R4z6v"
+   }
+   , {
+      img: "https://i.imgur.com/AGpCnAL.png",
+      videoURL: "./assets/videos/PurplePlanet.mp4",
+      title: "Purple Planet",
+      author: "Michal Kváč",
+      license: ""
+   }
+   , {
+      img: "https://i.imgur.com/361nJrU.jpg",
+      videoURL: "./assets/videos/Aqsa.mp4",
+      title: "Night Aqsa",
+      author: "Waffle Ranger",
+      license: ""
+   }
+   , {
+      img: "https://i.imgur.com/KqkEKmn.jpg",
+      videoURL: "./assets/videos/Fort.mp4",
+      title: "Fort",
+      author: "Gabriel Nascimento",
+      license: " https://www.artstation.com/artwork/GGNXB"
+   }
+   , {
+      img: "https://i.imgur.com/pMClfAa.jpg",
+      videoURL: "./assets/videos/Forest.mp4",
+      title: "Fall Forest",
+      author: "Waffle Ranger",
+      license: ""
+   }
+];
+
+export default tileData;
